1bc5b827e3199358c5149626bc9eeac945dff21b
tibov <tibo.verreycken@telenet.be>
2024-12-02 22:41:37 +0100

    Small progress in setting up the regression tests

--- testing/SnapshotMaker.cpp
+++ testing/SnapshotMaker.cpp
@@ -1,9 +1,11 @@
 
 #include <cstdlib>
-
 int main(){
+    system("> ../ini-tests/filelist \\;");
+    system("cd ../ini-1; find . -iregex \".*ini*\" -exec sh -c 'echo \"${1}\" | cut -c3-' _ {} >> ./../ini-tests/filelist \\;");
+    system("cd ../ini-1; find . -iregex \".*L2D*\" -exec cp {} ./../ini-tests/ \\;");
 
 
     system("cd ../ini-tests; ./../cmake-build-debug/engine");
-    system("cd ../ini-tests; find . -iregex \".*bmp\" -exec cp {} ./../ini-snapshots/ \\;");
+    system("cd ../ini-tests; find . -iregex \".*bmp\" -exec mv {} ./../ini-snapshots/ \\;");
 }
